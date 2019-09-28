6180e45ab096edf1c3ecbd059e59663afe0cff03
nandini soni <nandini.soni8@gmail.com>
2019-09-28 16:30:31 +0530

    Running code

--- server.py
+++ server.py
@@ -6,3 +6,12 @@ port = 5000
 server_socket = socket.socket()
 server_socket.bind((host, port))
 server_socket.listen()
+conn, address = server_socket.accept()
+print("Connection from: " + str(address))
+while True:
+    data = conn.recv(1024).decode()
+    print("from connected user: " + str(data))
+    data = input(' -> ')
+    conn.send(data.encode())
+
+conn.close()
